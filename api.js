096cdc480af26c04e68d3da9a8e37bdc0a88fadc
musaddekali <musaddekali.dev@gmail.com>
2022-04-26 14:36:44 +0600

    projects page added

---
+++ api.js
@@ -0,0 +1,165 @@
+const webApi = {
+    navLink: [
+        {
+            id: 1,
+            name: 'home',
+            link: '/'
+        },
+        {
+            id: 2,
+            name: 'about',
+            link: '/'
+        },
+        {
+            id: 3,
+            name: 'services',
+            link: 'services'
+        },
+        {
+            id: 4,
+            name: 'skills',
+            link: 'skills'
+        },
+        {
+            id: 5,
+            name: 'projects',
+            link: 'projects'
+        },
+        {
+            id: 6,
+            name: 'contact',
+            link: 'contact'
+        },
+    ],
+    hero: {
+        title: 'Im web developer musaddek ali',
+        subTitle: 'Get every single solutions',
+        desc: 'Almost 2+ year of experience with Front End Web technologys like HTML, CSS, JavaScript, React js etc. I provide powerfull and fast website for your dream project.',
+        linkOne: 'learnMore',
+        linkTwo: 'HireMe'
+    },
+    services: [
+        {
+            id: 1,
+            icon: 'code',
+            title: 'Clean And Maintainable Code.',
+            desc: 'You website code will be more maintainabe and reuseable. This will give you opportunity to easyly handle your website with learge scale.',
+        },
+        {
+            id: 2,
+            icon: 'react',
+            title: 'Single Page Application(SPA)',
+            desc: 'Single Page Application (SPA). Provide progressive web application for your business or personal blog. This feature will provide you super fast website.',
+        },
+        {
+            id: 3,
+            icon: 'database',
+            title: 'Back End With Firebase',
+            desc: 'Store your data with firebase. You can store Text, Image, Video etc. in online. This is how you can access you data world wide.',
+        },
+        {
+            id: 4,
+            icon: 'global',
+            title: 'Website Live On Internet',
+            desc: 'You will able to see your website from anywhere in the world and share your wesite link to others.',
+        },
+    ],
+    skills: [
+        {
+            id: 1,
+            icon: 'icon',
+            title: 'Programming Languages',
+            items: ['javascript'],
+        },
+        {
+            id: 2,
+            icon: 'icon',
+            title: 'Libraries & Frameworks',
+            items: ['react', 'bootstrap', 'material ui'],
+        },
+        {
+            id: 3,
+            icon: 'icon',
+            title: 'Databases',
+            items: ['firebase'],
+        },
+        {
+            id: 4,
+            icon: 'icon',
+            title: 'Dev Tools',
+            items: ['git', 'github', 'browser dev tools', 'webpack', 'rest api etc'],
+        },
+        {
+            id: 5,
+            icon: 'icon',
+            title: 'design tools',
+            items: ['figma', 'adobi'],
+        },
+        {
+            id: 6,
+            icon: 'icon',
+            title: 'Miscellaneous',
+            items: ['HTML', 'CSS3', 'CLI', 'SASS', 'vs code', 'problem solving', 'markdown etc'],
+        },
+    ],
+    projects: [
+        {
+            id: 1,
+            img: 'img',
+            title: 'techMahid',
+            dese: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni placeat velit, esse accusamus facilis.',
+            tags: ['react', 'firebase', 'javascript', 'bootstrap', 'HTML', 'CSS3'],
+            codeLink: 'codelink',
+            previewLink: 'previewlink',
+        },
+        {
+            id: 1,
+            img: 'img',
+            title: 'techMahid',
+            dese: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni placeat velit, esse accusamus facilis.',
+            tags: ['react', 'firebase', 'javascript', 'bootstrap', 'HTML', 'CSS3'],
+            codeLink: 'codelink',
+            previewLink: 'previewlink',
+        },
+        {
+            id: 1,
+            img: 'img',
+            title: 'techMahid',
+            dese: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni placeat velit, esse accusamus facilis.',
+            tags: ['react', 'firebase', 'javascript', 'bootstrap', 'HTML', 'CSS3'],
+            codeLink: 'codelink',
+            previewLink: 'previewlink',
+        },
+        {
+            id: 1,
+            img: 'img',
+            title: 'techMahid',
+            dese: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni placeat velit, esse accusamus facilis.',
+            tags: ['react', 'firebase', 'javascript', 'bootstrap', 'HTML', 'CSS3'],
+            codeLink: 'codelink',
+            previewLink: 'previewlink',
+        },
+    ],
+    contact: [
+        {
+            id: 1,
+            icon: 'icon',
+            title: 'my address',
+            desc: 'Sylhet, Bangladesh'
+        },
+        {
+            id: 2,
+            icon: 'icon',
+            title: 'email me',
+            desc: 'musaddekali.dev@gmail.com'
+        },
+        {
+            id: 3,
+            icon: 'icon',
+            title: 'call me',
+            desc: '01404211790'
+        },
+    ],
+};
+
+export default webApi;
